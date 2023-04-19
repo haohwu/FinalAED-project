fefc9844b6c8b2cbb60d7510f61caea9aecf4c45
haohwu <wu.haoh@northeastern.edu>
2023-04-19 16:12:14 -0400

    Update userDirectory class

---
+++ FinalProjectADE/src/User/UserDirectory.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author whh
+ */
+public class UserDirectory {
+    private ArrayList<User> userList;
+    
+    public UserDirectory(){
+        userList = new ArrayList<>();
+    }
+
+    public ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(ArrayList<User> userList) {
+        this.userList = userList;
+    }
+    
+    
+}
