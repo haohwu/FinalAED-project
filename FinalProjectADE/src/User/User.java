f9ffee3df185f9468ca061dab0d3717d17afb2dd
haohwu <wu.haoh@northeastern.edu>
2023-04-19 16:10:32 -0400

    Update user class
    This class for any user who user our system

---
+++ FinalProjectADE/src/User/User.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+
+/**
+ *
+ * @author whh
+ */
+public class User {
+    private String name;
+    private String password;
+    
+    public User(){
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
+}
