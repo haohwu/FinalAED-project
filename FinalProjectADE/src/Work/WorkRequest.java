94814e180accecf392a2e41d5d2de5b2a1ddcb41
haohwu <wu.haoh@northeastern.edu>
2023-04-19 16:23:57 -0400

    Update workRequest class

---
+++ FinalProjectADE/src/Work/WorkRequest.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Work;
+
+import User.User;
+import java.util.Date;
+
+/**
+ *
+ * @author whh
+ */
+public class WorkRequest {
+    private User sender;
+    private User receiver;
+    private String discription;
+    private String status;
+    private Date startDate;
+    private Date endDate;
+    
+    public WorkRequest(){
+        this.startDate = new Date();
+        this.endDate = new Date();
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    
+    
+}
