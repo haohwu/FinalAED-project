8935b4154deec2e34996a0922204494dcd4babd7
Pratik Poojari <poojari.pr@northeastern.edu>
2023-04-19 18:28:43 -0400

    RequestJPanel

--- Finalaed/src/userinterface/MainJFrame.java
+++ Finalaed/src/userinterface/MainJFrame.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-//package userinterface;
+package userinterface;
 
 import Business.ConfigureASystem;
 import Business.Customer.Customer;
