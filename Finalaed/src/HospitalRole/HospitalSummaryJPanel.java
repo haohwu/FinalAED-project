80a82dfe7192e7614bb55ec6641e84cc26f0c772
Cheng-Yen Lai <lai.cheng@northeastern.edu>
2023-04-20 15:53:14 -0400

    update HospitalOrderAction

--- Finalaed/src/HospitalRole/HospitalSummaryJPanel.java
+++ Finalaed/src/HospitalRole/HospitalSummaryJPanel.java
@@ -174,7 +174,56 @@ public class HospitalSummaryJPanel extends javax.swing.JPanel {
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 
+    private void populateMedicine1() {
+        DefaultTableModel dtm = (DefaultTableModel)HospMedicineTable.getModel();
+        dtm.setRowCount(0);
+        if(pharma.getMedicineCatalog().getMedicineItemList() != null)
+        {
+            for(MedicineItem mi : pharma.getMedicineCatalog().getMedicineItemList())
+            {
+                Object[] row = new Object[dtm.getColumnCount()];
+                row[0] = mi.getId();
+                row[1] = mi;
+                row[2] = mi.getPrice();
+                dtm.addRow(row);
+            }
+        }
+        if(dtm.getRowCount() == 0)
+            {
+                AddHospMedicineItemButton.setEnabled(false);
+                HOADeleteItemButton.setEnabled(false);
+            }
+    }
 
+    private void populateOrder1() {
+        DefaultTableModel dtm = (DefaultTableModel)HOAOrderTable.getModel();
+        dtm.setRowCount(0);
+        if(cart != null)
+        {
+            HOADeleteItemButton.setEnabled(true);
+            HOAOrderButton.setEnabled(true);
+            int count = 1;
+            for(MedicineItem mi3 : cart)
+            {
+                Object[] row = new Object[dtm.getColumnCount()];
+                row[0] = count;
+                row[1] = mi3;
+                row[2] =  mi3.getPrice();
+                dtm.addRow(row);
+                count++;
+            }
+            HOAOrderTotalAmountText.setText(Integer.toString(totalAmount));
+        }
+        if(HOAOrderTable.getRowCount() <= 0)
+        {
+            HOAOrderButton.setEnabled(false);
+            HOADeleteItemButton.setEnabled(false);
+        }
+
+
+
+
+    }
 
 
 
