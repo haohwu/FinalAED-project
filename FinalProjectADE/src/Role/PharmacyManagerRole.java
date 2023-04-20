0b86366748a379b3685dee668e76074873427ad6
haohwu <wu.haoh@northeastern.edu>
2023-04-20 12:54:01 -0400

    Update PharmacyManager class

---
+++ FinalProjectADE/src/Role/PharmacyManagerRole.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Role;
+
+/**
+ *
+ * @author whh
+ */
+public class PharmacyManagerRole extends Role{
+    private RoleType roleType;
+    
+    public PharmacyManagerRole(){
+        RoleType type = RoleType.PharmacyManager;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+    
+    
+}
