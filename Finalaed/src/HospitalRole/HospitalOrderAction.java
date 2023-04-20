118a488b9b4676ae119959960a6f47180c393f35
Cheng-Yen Lai <lai.cheng@northeastern.edu>
2023-04-20 15:06:14 -0400

    update HospitalOrderAction

--- Finalaed/src/HospitalRole/HospitalOrderAction.java
+++ Finalaed/src/HospitalRole/HospitalOrderAction.java
@@ -1,10 +1,54 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package userinterface.HospitalRole;
+
+import Business.Customer.Customer;
+import Business.Hospital.Hospital;
+import Business.MedicineItems.MedicineItem;
+import Business.Orders.Orders;
+import Business.Pharmacy.Pharmacy;
+import java.awt.CardLayout;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+
 
 /**
  *
  * @author cylai
  */
-public class HospitalOrderAction {
+public class HospitalOrderAction extends javax.swing.JPanel{
     
+    JPanel userProcessContainer;
+    Customer customer;
+    Pharmacy pharma;
+    Orders orders;
+    int totalAmount = 0;
+    Hospital hospital;
+    ArrayList<MedicineItem> cart = new ArrayList<MedicineItem>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
