a050278dd2c739a4967b3d0cd6de247e495cac0d
haohwu <wu.haoh@northeastern.edu>
2023-04-21 00:53:07 -0400

    Update MedicineSupplierDirectory class

---
+++ FinalProjectADE/src/Medicine/MedicineSupplierDirectory.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Medicine;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author whh
+ */
+public class MedicineSupplierDirectory {
+    private ArrayList<MedicineSupplier> medicineSupplierList;
+    
+    public MedicineSupplierDirectory(){
+        medicineSupplierList = new ArrayList<>();
+    }
+
+    public ArrayList<MedicineSupplier> getMedicineSupplierList() {
+        return medicineSupplierList;
+    }
+
+    public void setMedicineSupplierList(ArrayList<MedicineSupplier> medicineSupplierList) {
+        this.medicineSupplierList = medicineSupplierList;
+    }
+    
+    
+}
