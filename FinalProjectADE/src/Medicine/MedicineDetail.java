afff502335f41bd01c28fe0d26e7ec9e82c6c23d
haohwu <wu.haoh@northeastern.edu>
2023-04-21 00:23:16 -0400

    Update MedicineDetail class
    Update

---
+++ FinalProjectADE/src/Medicine/MedicineDetail.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Medicine;
+
+/**
+ *
+ * @author whh
+ */
+public class MedicineDetail {
+    private String Id;
+    private String name;
+    private String composition;
+    private String symptom;
+    
+    public MedicineDetail(){
+        
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComposition() {
+        return composition;
+    }
+
+    public void setComposition(String composition) {
+        this.composition = composition;
+    }
+
+    public String getSymptom() {
+        return symptom;
+    }
+
+    public void setSymptom(String symptom) {
+        this.symptom = symptom;
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
+}
